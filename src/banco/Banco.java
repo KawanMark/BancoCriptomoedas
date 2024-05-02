f750dfd0ccc5123f31a8fde187c38d3ff97860f2
Kawan <kawandrew14@gmail.com>
2024-05-01 21:06:07 -0300

    Inicio do projeto banco criptmoedas, na qual estou realizando o desing

---
+++ src/banco/Banco.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package banco;
+
+import view.JanelaLogin;
+
+/**
+ *
+ * @author kawan
+ */
+public class Banco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       JanelaLogin J = new JanelaLogin();
+       J.setVisible(true);
+    }
+    
+}
